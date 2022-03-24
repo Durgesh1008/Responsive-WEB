55980bd63f6ccc8a86e1fa7cdcdfe6101afcb625
Durgeshchaudhary001 <96628196+Durgesh1008@users.noreply.github.com>
2022-03-24 18:11:46 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,11 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+rightNav = document.querySelector('.rightNav')
+
+
+burger.addEventListener('click' , ()=>{
+   rightNav.classList.toggel('v-class-resp');
+   nav-list.classList.toggel('v-class-resp');
+   navbar.classList.toggel('h-nev-resp');
+})
